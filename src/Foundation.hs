3804b8439fd6b7e92a6a1380044a68b31534135d
Connor <connor@richardconnorjohnstone.com>
2019-11-10 21:19:54 -0700

    I think I'm getting closer

--- src/Foundation.hs
+++ src/Foundation.hs
@@ -184,9 +184,9 @@ instance Yesod App where
         -- you to use normal widget features in default-layout.
 
         pc <- widgetToPageContent $ do
-            addStylesheet $ StaticR css_bootstrap_css
-            $(widgetFile "default-layout")
-        withUrlRenderer $(hamletFile "templates/default-layout-wrapper.hamlet")
+            --addStylesheet $ StaticR css_bootstrap_css
+            $(widgetFile "old-layout")
+        withUrlRenderer $(hamletFile "templates/old-layout-wrapper.hamlet")
 
     -- The page to be redirected to when authentication is required.
     authRoute
